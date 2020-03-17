de7c565c8e7e5198f029e48fe9add672acabf301
unknown <luvumbu.n@gmail.com>
2020-03-17 07:48:11 +0100

    police

---
+++ index.php
@@ -0,0 +1,26 @@
+<style>
+
+ 
+
+
+
+
+
+
+
+
+
+
+@font-face {
+  font-family: "couture-bld";
+  src: url("couture-bld.otf");
+}
+
+body {
+  font-family:"couture-bld";
+  font-size:5em ; 
+} 
+</style>
+
+
+Bokonzi
